2cd6deeedef2fdf9344cb42dac81f15b9015c671
Z0ro0 <w2110@e-mirim.hs.kr>
2023-11-17 14:57:14 +0900

    레시피 앱 first commit

---
+++ recipe_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'layout example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipe Example'),
+      ),
+      body: Column(
+        children: [
+          Image.asset('dosirock.jpg'),
+          Padding(padding: EdgeInsets.all(24))
+        ],
+      ),
+    );
+  }
+}
