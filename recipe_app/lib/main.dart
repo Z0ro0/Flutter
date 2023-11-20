5b6d3ba7bae9940adf24556dc2f99d04ebaa0a7b
Z0ro0 <w2110@e-mirim.hs.kr>
2023-11-20 11:46:43 +0900

    간단한 레시피 화면 완성

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -50,7 +50,47 @@ class MyHomePage extends StatelessWidget {
       ],
     );
 
+Widget stepSection = Padding(
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('조리 순서',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(//text위젯에 여백주기
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Text(
+                "1",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            SizedBox(//text위젯에 크기 지정해 줌
+              width: MediaQuery.of(context).size.width*0.5,
+              child: Text(
+                '[도시락] 흰 밥에 참기름 넣고 섞으세여 ㅎㅎ 뜨거우니까 조심',
+                style: TextStyle(fontSize: 14.0),
+              ),
+            ),
+            Image.asset(
+              'bob.jpg',
+              width: MediaQuery.of(context).size.width*0.15,//화면 너비의 15%로 사이즈 조정
+              fit: BoxFit.contain,//이미지의 가로 세로 비율 유지
+            ),
 
+          ],
+        ),
+        buildiconLabelRow(Icons.lightbulb, '찬밥써두괜차나여', Colors.teal),
+        buildiconLabelRow(Icons.shopping_cart, '완전차가운흰밥', Colors.grey),
+
+      ],
+    ),
+);
 
 
     return Scaffold(
@@ -62,6 +102,7 @@ class MyHomePage extends StatelessWidget {
           Image.asset('assets/dosirock.jpg'),
           titleSection,//재사용 메서드 호출
           iconSection,//이거 넣으면 또 왜... 가운데 정렬...
+          stepSection
         ],
       ),
     );
