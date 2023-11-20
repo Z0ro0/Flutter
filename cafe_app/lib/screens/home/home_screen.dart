a3732929e9906b4ee0bb2b5677094b0720df032d
Z0ro0 <w2110@e-mirim.hs.kr>
2023-11-20 13:35:55 +0900

    배너 생성 후 파일 분리

--- cafe_app/lib/screens/home/home_screen.dart
+++ cafe_app/lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:cafe_app/widgets/banner_widget.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -13,27 +15,46 @@ class MyHomePage extends StatelessWidget {
      "케이크",
    ];
 
+   List<String> bannerItemImgUrl = [
+     "assets/images/banner01.jpg",
+     "assets/images/banner02.jpg",
+   ];
+
    //2. 탭바 컨트롤러 생성
    return DefaultTabController(
        length: menuItems.length,
        child: Scaffold(
          appBar: AppBar(
-           title: Text("메뉴"),
+           title: const Text("메뉴", style: TextStyle(color: Colors.black),
+           ),
+           centerTitle: true,
+           backgroundColor: Colors.white,
+           leading: Icon(
+             Icons.home,
+             color: Colors.grey,
+           ),
            //3, 탭바 생성(탭바 아래 각각의 탭 생성)
            bottom: TabBar(
-             tabs: List.generate(
+             tabs: List.generate(//menuitems 길이만큼 탭바 메뉴 생성
                  menuItems.length,
                      (index) => Tab(
                        text: menuItems[index],
                      ),
              ),
+             unselectedLabelColor: Colors.black38,//선택되지 않은 탭 진한 회색
+             labelColor: Colors.black,//기본 탭 색은 검정색
+             indicatorColor: Colors.black,//탭 밑의 선 색 검정색
+             indicatorSize: TabBarIndicatorSize.label,//탭 밑의 선 크기는 라벨 크기
+             isScrollable: true,//탭바 스크롤 가능하도록
            ),
          ),
          //4.탭바 뷰 생성
          body: TabBarView(
            children: [
-             Center(
-               child: Text("New 화면입니다"),
+             Column(
+               children: [
+                 BannerWidget(bannerItemImgUrl: bannerItemImgUrl)
+               ],
              ),
              Center(
                child: Text("커피&음료 화면입니다"),
