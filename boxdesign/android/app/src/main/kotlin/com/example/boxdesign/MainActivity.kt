aa3bf4ed51d23c441cf4ce9ad756dafed6fd67d6
Z0ro0 <w2110@e-mirim.hs.kr>
2023-11-20 17:33:04 +0900

    메인 페이지 배너

--- boxdesign/android/app/src/main/kotlin/com/example/boxdesign/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.boxdesign
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- danggenmarketlayout/android/app/src/main/kotlin/com/example/danggenmarketlayout/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.danggenmarketlayout
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
